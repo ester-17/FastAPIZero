3f6efe5a6c029398b59067ae370373f0f2ab9485
ester-17 <139085601+ester-17@users.noreply.github.com>
2025-10-08 22:24:59 -0300

    fineshed class 01

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,16 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
 
 client = TestClient(app)
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)    # Arrange (organização)
+
+    response = client.get('/')  # Act (ação): chama a função com o endpoint '/'
+
+    assert response.status_code == HTTPStatus.OK  # Assert (afirmar)
+    assert response.json() == {'message': 'Hello World!'}
