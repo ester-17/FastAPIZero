dc6d86eff9a6574d05abfd89b49f300d5b655e5a
ester-17 <139085601+ester-17@users.noreply.github.com>
2025-10-19 20:49:23 -0300

    first commit

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,20 +1,10 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-
+from fastapi_zero.schemas import Message
 app = FastAPI()
 
 
-@app.get('/', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return '''
-    <html>
-        <head>
-            <title> Nosso Olá mundo!</title>
-        </head>
-        <body>
-            <h1> Olá Mundo! </h1>
-        </body>
-    </html>
-    '''
+    return {'message': 'Olá Mundo!'}
